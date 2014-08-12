14a8cbcbfede2f8f055bb7dca94be87a22aa4390
Michael Heard <compmike@gmail.com>
2014-08-12 19:26:58 -0400

    added some images and linked them on my page, added time stamp based on cookies on every page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :add_cookie
+  def add_cookie
+  	#get the current date and time
+  	#create a string version of date and time
+  	current_dt = Time.now.strftime("%B %d, %Y, %A %I:%M:%S %p")
+  	#store the current date and time in the session, if it's our first visit
+  	if session['first_visit'] == nil
+  		session['first_visit'] = current_dt
+  	end
+  	#pull the date and time from the session and make it avaialble to the view.
+  	@first_visit = session['first_visit']
+
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+  # def index
+  # 	#get the current date and time
+  # 	#create a string version of date and time
+  # 	current_dt = Time.now.strftime("%B %d, %Y, %A %I:%M:%S %p")
+
+  # 	#store the current date and time in the session, if it's our first visit
+  # 	if session['first_visit'] == nil
+  # 		session['first_visit'] = current_dt
+  # 	end
+  # 	#pull the date and time from the session and make it avaialble to the view.
+  # 	@first_visit = session['first_visit']
+
+  # end
 end
