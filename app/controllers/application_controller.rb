511813a9b632f45455726b444cc0baff226bf81e
Michael Heard <compmike@gmail.com>
2014-08-13 17:42:47 -0400

    changed before_filter to before_action, it appears before_filter is soft of old

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :add_cookie
+  before_action :add_cookie
   def add_cookie
   	#get the current date and time
   	#create a string version of date and time
